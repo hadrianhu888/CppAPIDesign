c8a5916783fcf89545f1998915eaf43833014023
hadrianhu888 <hadrian.hu@gmail.com>
2023-10-27 22:47:03 -0400

    Added CppAdapter pattern

---
+++ MyWork/Chapter3/CppAdapter/src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-10-27
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <iostream>
+
+#include "main.h"
+
+int main(int argc, char **argv) {
+  std::cout << "Hello World!" << std::endl;
+  CppAdapter adapter;
+  adapter.DoSomething();
+  adapter.DoSomethingElse();
+  adapter.DoSomethingMore();
+  adapter.~CppAdapter();
+  return 0;
+}
